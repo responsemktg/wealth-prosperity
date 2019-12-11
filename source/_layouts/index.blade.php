8f4a46674d14f492dc7bea51bebadaa3224268f4
Marc Broad <mbroad@Marcs-MacBook-Pro.local>
2019-12-10 23:20:53 -0500

    initial

---
+++ source/_layouts/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="{{ $page->meta_description ?? $page->siteDescription }}">
+
+        <meta property="og:title" content="{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:description" content="{{ $page->siteDescription }}" />
+
+        <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
+
+        <link rel="home" href="{{ $page->baseUrl }}">
+        <link rel="icon" href="/favicon.ico">
+
+        <script type='text/javascript' src='//www.midijs.net/lib/midi.js'></script>
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    </head>
+
+    <body class="">
+      
+        @yield('body')
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
+        @stack('scripts')
+    </body>
+</html>
